ef7c4da90087cb189b462c2ac2a9b90123ac4051
saqib92 <najam1992@gmail.com>
2020-09-13 19:56:06 +0500

    Update User API with Put

--- router.ts
+++ router.ts
@@ -1,5 +1,5 @@
 import { Router} from 'https://deno.land/x/oak/mod.ts';
-import {getActivities, getActivity, addActivity, updateActivity, deleteActivity, testCode, addUser, getUsers, getUser} from './controllers/activity-contoller.ts';
+import {getActivities, getActivity, addActivity, updateActivity, deleteActivity, testCode, addUser, getUsers, getUser, updateUser} from './controllers/activity-contoller.ts';
 
 const router = new Router();
 
@@ -8,9 +8,11 @@ router.get('/api/v1/activities/:id', getActivity)
 router.post('/api/v1/activities', addActivity)
 router.put('/api/v1/activities/:id', updateActivity)
 router.delete('/api/v1/activities/:id', deleteActivity)
+
 router.get('/test', testCode);
 router.post('/adduser', addUser);
 router.get('/getusers', getUsers);
 router.get('/getuser/:id', getUser);
+router.put('/updateuser/:id', updateUser)
 
 export default router;
